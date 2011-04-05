3b584f62d69b28614bb6d1d6d70ea2a6d7deeb8f
Ulrika Sjoberg <ulrika.sjoberg@stericsson.com>
2011-04-05 16:52:20 +0200

    Remove use of ALL_PREBUILT
    Removes the use of ALL_PREBUILT. Use of this parameter will be
    forbidden in future releases of Android and it also prevents
    builds of Android SDK.
    
    Depends-On: I9b44253b4adf225da8ca449030c00f18fd19a637
    
    ST-Ericsson ID: 328774, 321316
    
    ST-Ericsson FOSS-OUT ID: NA
    
    Change-Id: I931aee14ace7930344e83bc3bc0de7028b3f67ed
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/20088
    Reviewed-by: Hans ODEBERG <hans.odeberg@stericsson.com>
    Reviewed-by: Markus GRAPE <markus.grape@stericsson.com>
    Reviewed-by: Johan LARSSON <johan.j.larsson@stericsson.com>
    Tested-by: Ulrika SJOBERG <ulrika.sjoberg@stericsson.com>
    Reviewed-by: Ulrika SJOBERG <ulrika.sjoberg@stericsson.com>

--- Android.mk
+++ Android.mk
@@ -11,20 +11,33 @@ ifeq ($(NWM_ENABLE_FEATURE_MODEMFS),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-.PRECIOUS: $(PRODUCT_OUT)/modemfs.img
-ALL_PREBUILT += $(PRODUCT_OUT)/modemfs.img
-ALL_MODULES += $(PRODUCT_OUT)/modemfs.img
-$(PRODUCT_OUT)/modemfs.img: $(LOCAL_PATH)/modemfs $(MKEXT2USERIMG) $(MAKE_EXT4FS)
+PRIVATE_MODEM_PATH := $(LOCAL_PATH)
+
+TMP_MODEMFS := $(TARGET_OUT_INTERMEDIATES)/tmp_modemfs
+$(shell mkdir -p $(TMP_MODEMFS))
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(TMP_MODEMFS)
+LOCAL_SRC_FILES := modemfs.img
+LOCAL_MODULE := modemfs.img
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+
+$(addprefix $(LOCAL_PATH),/$(LOCAL_SRC_FILES)): $(PRIVATE_MODEM_PATH)/modemfs $(MKEXT2USERIMG) $(MAKE_EXT4FS)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
 	$(call build-userimage-ext-target,$<,$@,modemfs,ext4,$(BOARD_MODEMIMAGE_PARTITION_SIZE))
 else
 	$(hide) echo "ERROR: File system image $@ has only been verified to work as an ext4 file system image!"; false
 endif
-endif
+
+include $(BUILD_PREBUILT)
 
 # Copies modem images to PRODUCT_OUT/modem_images
-modem_images := $(wildcard $(LOCAL_PATH)/*.fs)
-modem_images += $(wildcard $(LOCAL_PATH)/*_ipl_*)
+modem_images := $(wildcard $(PRIVATE_MODEM_PATH)/*.fs)
+modem_images += $(wildcard $(PRIVATE_MODEM_PATH)/*_ipl_*)
 
-copy_modem_images := $(foreach modem,$(subst $(LOCAL_PATH),,$(modem_images)),$(LOCAL_PATH)/$(modem):modem_images/$(modem))
+copy_modem_images := $(foreach modem,$(subst $(PRIVATE_MODEM_PATH),,$(modem_images)),$(PRIVATE_MODEM_PATH)/$(modem):modem_images/$(modem))
 PRODUCT_COPY_FILES += $(copy_modem_images)
+
+endif #NWM_ENABLE_FEATURE_MODEMFS
